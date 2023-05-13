9d2d9dffa38b7e55fc64b303d7760fbfac80fe3e
Atsuhiro <atcheri@gmail.com>
2023-05-13 23:41:55 +0200

    feat: add newsletter component
    #1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Carousel from "./components/Hero/Carousel";
 import Hero from "./components/Hero";
+import NewsLetter from "./components/NewsLetter";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Carousel />
+      <NewsLetter />
     </>
   );
 }
