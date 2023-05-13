9ae98d0e5f47e885e75c4152e33b3d5aac7c83e5
Match <massahiro.endo@gmail.com>
2023-05-13 12:02:20 +0200

    feat: move carousel from hero to home

--- src/app/components/Hero/index.tsx
+++ src/app/components/Hero/index.tsx
@@ -1,4 +1,3 @@
-import Carousel from "./Carousel";
 import Link from "next/link";
 import { ROUTES } from "@/constants/routes";
 
@@ -7,19 +6,16 @@ const message = "sub-title here";
 
 const Hero = () => {
   return (
-    <>
-      <div className="flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover bg-hero-pattern">
-        <div className="absolute top-0 left-0 right-0 bottom-0 bg-black/70 z-[2]" />
-        <div className="flex flex-col gap-3 text-white z-[2] mt-[-10rem]">
-          <h2 className="text-5xl font-bold">{heading}</h2>
-          <p className="text-xl">{message}</p>
-          <Link href={ROUTES.STORE.path}>
-            <button className="px-8 py-2 border">Here</button>
-          </Link>
-        </div>
+    <div className="flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover bg-hero-pattern">
+      <div className="absolute top-0 left-0 right-0 bottom-0 bg-black/70 z-[2]" />
+      <div className="flex flex-col gap-3 text-white z-[2] mt-[-10rem]">
+        <h2 className="text-5xl font-bold">{heading}</h2>
+        <p className="text-xl">{message}</p>
+        <Link href={ROUTES.STORE.path}>
+          <button className="px-8 py-2 border">Here</button>
+        </Link>
       </div>
-      <Carousel />
-    </>
+    </div>
   );
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Carousel from "./components/Hero/Carousel";
 import Hero from "./components/Hero";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <Carousel />
     </>
   );
 }
