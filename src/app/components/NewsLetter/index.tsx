bf328e63db9c23e5ba81280c13e774bcc39f7520
Atsuhiro <atcheri@gmail.com>
2023-05-14 09:55:44 +0200

    feat: add news-letter block and send email
    Using sendgrid service to send email
    
    #1

--- src/app/components/NewsLetter/index.tsx
+++ src/app/components/NewsLetter/index.tsx
@@ -1,8 +1,25 @@
+"use client";
+
+import { Button, Toast } from "flowbite-react";
+import { FC, useState } from "react";
+
 import { BsCalendarRange } from "react-icons/bs";
-import { FC } from "react";
 import { MdOutlineFrontHand } from "react-icons/md";
+import { RiMailUnreadLine } from "react-icons/ri";
+import { sendNewsLetterEmail } from "./actions";
 
 const NewsLetter: FC = () => {
+  const [loading, setLoading] = useState(false);
+  const [showToast, setShowToast] = useState(false);
+
+  const handleNewsLetterSubmit = async (data: FormData) => {
+    setLoading(() => true);
+    const email = data.get("email") as string;
+    await sendNewsLetterEmail(email);
+    setLoading(() => false);
+    setShowToast(() => true);
+  };
+
   return (
     <div className="relative isolate overflow-hidden bg-gray-900 py-16 sm:py-24 lg:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
@@ -15,7 +32,10 @@ const NewsLetter: FC = () => {
               Nostrud amet eu ullamco nisi aute in ad minim nostrud adipisicing
               velit quis. Duis tempor incididunt dolore.
             </p>
-            <form className="mt-6 flex max-w-md gap-x-4">
+            <form
+              action={handleNewsLetterSubmit}
+              className="mt-6 flex max-w-md gap-x-4"
+            >
               <label htmlFor="email-address" className="sr-only">
                 Email address
               </label>
@@ -27,12 +47,9 @@ const NewsLetter: FC = () => {
                 className="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-blue-500 sm:text-sm sm:leading-6"
                 placeholder="Enter your email"
               />
-              <button
-                type="submit"
-                className="flex-none rounded-md border-blue-700 border px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-blue-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500"
-              >
+              <Button type="submit" disabled={loading} isProcessing={loading}>
                 Subscribe
-              </button>
+              </Button>
             </form>
           </div>
           <dl className="grid grid-cols-1 gap-x-8 gap-y-10 sm:grid-cols-2 lg:pt-2">
@@ -59,6 +76,19 @@ const NewsLetter: FC = () => {
           </dl>
         </div>
       </div>
+      {showToast && (
+        <div className="mt-6 flex justify-center">
+          <Toast>
+            <div className="flex justify-center gap-3 px-4 text-sm font-normal">
+              <div className="rounded-md bg-white/5 p-2 ring-1 ring-white/10">
+                <RiMailUnreadLine className="h-6 w-6 text-blue-600" />
+              </div>{" "}
+              Thank you for subscribing to your news-letter
+            </div>
+            <Toast.Toggle />
+          </Toast>
+        </div>
+      )}
       <div
         className="absolute left-1/2 top-0 -z-10 -translate-x-1/2 blur-3xl xl:-top-6"
         aria-hidden="true"
