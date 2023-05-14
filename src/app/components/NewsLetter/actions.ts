fb32b7b394a37d46a51ca9ff9ddc2276bd7737cf
Atsuhiro <atcheri@gmail.com>
2023-05-14 09:55:44 +0200

    feat: add news-letter block and send email
    Using sendgrid service to send email
    
    #1

---
+++ src/app/components/NewsLetter/actions.ts
@@ -0,0 +1,33 @@
+"use server";
+
+import WelcomeNewsLetterEmail from "../../../../emails/WelcomeNewsLetter";
+import { render } from "@react-email/render";
+import sendgrid from "@sendgrid/mail";
+import validator from "email-validator";
+
+const apiKey = process.env.SENDGRID_API_KEY;
+
+if (!apiKey) {
+  throw Error(
+    "no Sendgrid api key environement variable defined. Please check your .env file"
+  );
+}
+sendgrid.setApiKey(apiKey);
+
+export const sendNewsLetterEmail = async (email: string): Promise<void> => {
+  if (!validator.validate(email)) {
+    throw Error("invalid email address");
+  }
+
+  const emailHtml = render(WelcomeNewsLetterEmail({ email: email.trim() }));
+
+  const options = {
+    from: "atcheri@gmail.com",
+    to: email,
+    subject: "Thank you for signing up to our news letter",
+    html: emailHtml,
+  };
+
+  sendgrid.send(options);
+  console.log("news letter email successfully sent to ", email);
+};
