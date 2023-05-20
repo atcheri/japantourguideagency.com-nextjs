c2095debde3c5c900fed54946c1e6ab69867f55f
Match <massahiro.endo@gmail.com>
2023-05-20 15:02:14 +0200

    feat: initial product comp integration

---
+++ src/app/components/Products/Products.tsx
@@ -0,0 +1,10 @@
+import ProductCard from "./ProductCard";
+import React from "react";
+
+type Props = {};
+
+const Products = (props: Props) => {
+  return <ProductCard />;
+};
+
+export default Products;
