fb9eb3c08c61ab53467fadfe57818b27ed42ab16
Atsuhiro <atcheri@gmail.com>
2023-05-23 23:48:41 +0200

    fix: background next to black filter div

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -3,9 +3,10 @@ const message = "And experience unforgettable moments of life";
 
 const Hero = () => {
   return (
-    <section className="bg-fixed bg-center bg-cover bg-hero-pattern">
-      <div className="container mx-auto px-3 max-md:-my-[29px]">
-        <div className="absolute top-0 left-0 right-0 bottom-0 bg-black/70 z-[2]" />
+    <section>
+      <div className="absolute w-full h-full bg-fixed bg-center bg-cover bg-hero-pattern" />
+      <div className="absolute w-full h-full bg-black/70 z-[2]" />
+      <div className="container mx-auto px-3">
         <div className="flex items-center h-screen">
           <div className="flex flex-col max-w-xl gap-5 z-[2] text-white">
             <h1 className="text-3xl font-bold md:text-5xl">{heading}</h1>
