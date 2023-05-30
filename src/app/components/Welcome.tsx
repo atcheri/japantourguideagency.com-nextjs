09b55abc728e274465e434db747967e4fdf7b4bb
Match <massahiro.endo@gmail.com>
2023-05-30 16:19:39 +0200

    fix: add welcome section content and style
    Co-authored-by: Atch <atcheri@users.noreply.github.com>

--- src/app/components/Welcome.tsx
+++ src/app/components/Welcome.tsx
@@ -46,7 +46,11 @@ const Welcome: FC<{ photos: string[] }> = ({
               but rather a transformational experience that can change your
               life.{" "}
             </p>
-            <div className={!showMore ? "gradient-mask-b-50" : ""}>
+            <div
+              className={`flex flex-col gap-2 ${
+                !showMore ? "gradient-mask-b-50" : ""
+              }`}
+            >
               <p>
                 We believe that travel is a pilgrimage that can help you
                 discover new perspectives, connect with different cultures, and
@@ -58,15 +62,49 @@ const Welcome: FC<{ photos: string[] }> = ({
                 personalized experiences for each of our clients.{" "}
               </p>
               {showMore && (
-                <p>
-                  We call them our &ldquo;&ldquo;star guides&ldquo;&ldquo;
-                  because they go above and beyond to ensure that every journey
-                  is exceptional.
-                </p>
+                <>
+                  <p>
+                    We call them our &ldquo;&ldquo;star guides&ldquo;&ldquo;
+                    because they go above and beyond to ensure that every
+                    journey is exceptional.
+                  </p>
+                  <p>
+                    We understand that each traveler has their own interests and
+                    preferences. That&ldquo;s why we offer personalized journeys
+                    that can be customized to suit your individual needs.
+                    Whether you&ldquo;re in search of the vibrant energy of
+                    Tokyo and Osaka, the rich cultural heritage of Kyoto and
+                    Nara, the stunning natural beauty of Okinawa and Hokkaido,
+                    or the awe-inspiring majesty of Mt. Fuji, we have a range of
+                    tours and itineraries that can be tailored to meet your
+                    specific interests and preferences.
+                  </p>
+                  <p>
+                    Our guides are not just knowledgeable about the history and
+                    culture of Japan, but they are also experts in finding
+                    hidden gems and off-the-beaten-path destinations that are
+                    not typically included in tourist itineraries. This ensures
+                    that each journey with us is unique and unforgettable.
+                  </p>
+                  <p>
+                    At our agency, we believe that travel should be an inclusive
+                    and sustainable experience. That&ldquo;s why we work with
+                    local communities and businesses to support responsible
+                    tourism and to help preserve Japan&ldquo;s natural and
+                    cultural heritage.
+                  </p>
+                  <p>
+                    We are committed to providing our clients with exceptional
+                    service and ensuring that every journey with us is
+                    memorable, transformative, and tailored to your needs.
+                    Contact us today to start planning your unforgettable
+                    journey through Japan with our star guides.
+                  </p>
+                </>
               )}
             </div>
 
-            <Button color="light" onClick={toggleWelcomeText}>
+            <Button color="light" onClick={toggleWelcomeText} className="mt-2">
               {showMore ? "Read less..." : "Read more..."}
             </Button>
           </div>
@@ -77,51 +115,3 @@ const Welcome: FC<{ photos: string[] }> = ({
 };
 
 export default Welcome;
-
-{
-  /* <div>
-    //   <p>
-    //     Welcome to our Japan Tour Guide agency, where we believe that travel is
-    //     more than just visiting a destination, but rather a transformational
-    //     experience that can change your life. We believe that travel is a
-    //     pilgrimage that can help you discover new perspectives, connect with
-    //     different cultures, and create lifelong memories. Our team of
-    //     specialized guides are not just experts in their fields, but they are
-    //     also passionate about creating unique and personalized experiences for
-    //     each of our clients. We call them our &ldquo;&ldquo;star
-    //     guides&ldquo;&ldquo; because they go above and beyond to ensure that
-    //     every journey is exceptional.
-    //   </p>
-    //   <p>
-    //     We understand that each traveler has their own interests and
-    //     preferences. That&lsquo;s why we offer personalized journeys that can be
-    //     customized to suit your individual needs. Whether you&lsquo;re in search
-    //     of the vibrant energy of Tokyo and Osaka, the rich cultural heritage of
-    //     Kyoto and Nara, the stunning natural beauty of Okinawa and Hokkaido, or
-    //     the awe-inspiring majesty of Mt. Fuji, we have a range of tours and
-    //     itineraries that can be tailored to meet your specific interests and
-    //     preferences.
-    //   </p>
-    //   <p>
-    //     Our guides are not just knowledgeable about the history and culture of
-    //     Japan, but they are also experts in finding hidden gems and
-    //     off-the-beaten-path destinations that are not typically included in
-    //     tourist itineraries. This ensures that each journey with us is unique
-    //     and unforgettable.
-    //   </p>
-    //   <p>
-    //     At our agency, we believe that travel should be an inclusive and
-    //     sustainable experience. That&lsquo;s why we work with local communities
-    //     and businesses to support responsible tourism and to help preserve
-    //     Japan&lsquo;s natural and cultural heritage.
-    //   </p>
-    //   <p>
-    //     We are committed to providing our clients with exceptional service and
-    //     ensuring that every journey with us is memorable, transformative, and
-    //     tailored to your needs. Contact us today to start planning your
-    //     unforgettable journey through Japan with our star guides.
-    //   </p>
-    // </div>
-
-    */
-}
